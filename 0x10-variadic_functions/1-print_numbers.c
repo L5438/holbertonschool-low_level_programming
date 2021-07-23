af661e2cdb8b497b2bc6f75d92c50ed9b531d9e8
L5438 <57583370+L5438@users.noreply.github.com>
2021-07-23 02:32:13 -0500

    Add files via upload

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Function that sumates all arguments
+ * @separator: String who contains ","
+ * @n: Number of values
+ * Return: Always 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	va_start(args, n);
+
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(args, int));
+		if (x != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(args);
+	printf("\n");
+}
